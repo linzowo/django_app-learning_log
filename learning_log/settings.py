8d9c3fe9a9c05a85ec7990bce3bfacad9e8c763c
linzowo <linzowo@qq.com>
2018-10-02 22:08:52 +0800

    添加debug

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -151,6 +151,8 @@ if os.getcwd() == '/app':
     #支持所有主机头
     ALLOWED_HOSTS = ['*']
 
+    DEBUG = False
+
     #静态资产配置
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = 'staticfiles'
